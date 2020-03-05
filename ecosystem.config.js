e3c952d4251bd1d19a9f0c9edbe10679a03dce49
jeremy <jeremy@chaufourier.fr>
2020-03-05 22:31:17 +0100

    Update deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     instances: 4,
     autorestart: true,
     watch: false,
-    max_memory_restart: '1G',
+    max_memory_restart: '512M',
     env: {
       NODE_ENV: 'development'
     },
